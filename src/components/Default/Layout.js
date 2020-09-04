3e44cc38c94fd85c40bdf20ab9a9748be33e64ba
Mike Capecci <mjcapecci@gmail.com>
2020-09-04 16:48:46 -0500

    updates default folder structure

---
+++ src/components/Default/Layout.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import "../../styles/index.scss"
+
+import SEO from "./seo"
+
+const Layout = ({ page, children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <SEO title={page} />
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built by
+        {` `}
+        <a href="https://www.gatsbyjs.org">Frontier Web Development</a>
+      </footer>
+    </>
+  )
+}
+
+export default Layout
