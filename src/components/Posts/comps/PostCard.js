43ec6daedc02b84647aa16cbba31a3eb99668bdf
Mike Capecci <mjcapecci@gmail.com>
2020-11-09 18:33:04 -0600

    adds more styles to pages

--- src/components/Posts/comps/PostCard.js
+++ src/components/Posts/comps/PostCard.js
@@ -7,12 +7,9 @@ const PostCard = ({ post }) => {
       <div class="card-body">
         <img src={post.node.frontmatter.featuredImage.publicURL} width="100%" />
         <h5 class="card-title">{post.node.frontmatter.title}</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <Link to={post.node.fields.slug} class="btn btn-1">
-          Go somewhere
+        <p class="card-text">{post.node.excerpt}</p>
+        <Link to={post.node.fields.slug} class="btn btn-2">
+          Read More
         </Link>
       </div>
     </div>
--- src/templates/blog-post-template.js
+++ src/templates/blog-post-template.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import Layout from '../components/Default/Layout'
-import Helmet from 'react-helmet'
+import React from "react"
+import Layout from "../components/Default/Layout"
+import Helmet from "react-helmet"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import { HowTo } from "../components/IndexSections/index"
 
 function BlogPostLayout({ pageContext }) {
-
-  const {title, author, date} = pageContext.frontmatter
-
-  console.log(pageContext)
+  const { title } = pageContext.frontmatter
 
   return (
     <Layout>
       <Helmet>
         <title>{title}</title>
       </Helmet>
-      <article>
-        <header>
-          <h1>{title}</h1>
-          <span>Author: {author}</span>
-          <time>Date: {date}</time>
-        </header>
-        <MDXRenderer>{pageContext.body}</MDXRenderer>
-      </article>
+      <section className="pattern article-section">
+        <article>
+          <header>
+            <h1>{title}</h1>
+          </header>
+          <MDXRenderer>{pageContext.body}</MDXRenderer>
+        </article>
+      </section>
+      <HowTo />
     </Layout>
   )
 }
